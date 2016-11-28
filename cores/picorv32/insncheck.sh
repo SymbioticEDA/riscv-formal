68013ef29bc9e1d8e4dfe79b816ea9f83e6bf12f
Clifford Wolf <clifford@clifford.at>
2016-11-28 19:49:53 +0100

    Added ABC bmc3 code to cores/picorv32/insncheck.sh

--- cores/picorv32/insncheck.sh
+++ cores/picorv32/insncheck.sh
@@ -6,5 +6,12 @@ yosys -p '
 	read_verilog -formal -D INSN_VH="addi.vh" -D CHANNEL_IDX=0 ../../insns/checker.v
 	prep -nordff -top testbench
 	write_smt2 -wires insncheck.smt2
+	# miter -assert -flatten testbench miter
+	# hierarchy -top miter
+	# setattr -unset keep
+	# memory_map; opt -full; techmap
+	# opt -fast; abc -fast;; stat
+	# write_blif insncheck.blif
 '
+# yosys-abc -c 'read_blif insncheck.blif; undc; strash; zero; bmc3 -F 25 -v'
 yosys-smtbmc -s yices -t 25 --dump-vcd output.vcd insncheck.smt2
