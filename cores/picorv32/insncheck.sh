7e0e2677e7705ed769177baf47ae1669cc02bbe9
Clifford Wolf <clifford@clifford.at>
2016-11-29 00:38:15 +0100

    Added addi, slti, sltiu insns

--- cores/picorv32/insncheck.sh
+++ cores/picorv32/insncheck.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
-set -ex
-yosys -p '
+
+solver="bmc3"
+insn="addi"
+
+while getopts s:i: opt; do
+	case "$opt" in
+		s) solver="$OPTARG";;
+		i) insn="$OPTARG";;
+		*) exit 1
+	esac
+done
+
+yosys_script="
 	read_verilog -formal insncheck.v
 	read_verilog -formal -norestrict -assume-asserts -D RISCV_FORMAL ../../../picorv32/picorv32.v
-	read_verilog -formal -D INSN_VH="addi.vh" -D CHANNEL_IDX=0 ../../insns/checker.v
+	read_verilog -formal -D INSN_VH=\"${insn}.vh\" -D CHANNEL_IDX=0 ../../insns/checker.v
 	prep -nordff -top testbench
-	write_smt2 -wires insncheck.smt2
-	# miter -assert -flatten testbench miter
-	# hierarchy -top miter
-	# setattr -unset keep
-	# memory_map; opt -full; techmap
-	# opt -fast; abc -fast;; stat
-	# write_blif insncheck.blif
-'
-# yosys-abc -c 'read_blif insncheck.blif; undc; strash; zero; bmc3 -F 25 -v'
-yosys-smtbmc -s yices -t 25 --dump-vcd output.vcd insncheck.smt2
+"
+
+case "$solver" in
+	bmc3)
+		yosys_script="$yosys_script
+			miter -assert -flatten testbench miter
+			hierarchy -top miter
+			setattr -unset keep
+			memory_map; opt -full; techmap
+			opt -fast; abc -fast;; stat
+			write_blif insncheck.blif
+		"
+		solver_cmd="yosys-abc -c 'read_blif insncheck.blif; undc; strash; zero; bmc3 -F 25 -v'"
+		;;
+	*)
+		yosys_script="$yosys_script
+			write_smt2 -wires insncheck.smt2
+		"
+		solver_cmd="yosys-smtbmc -s $solver -t 25 --dump-vcd output.vcd insncheck.smt2"
+		;;
+esac
+
+set -ex
+yosys -p "$yosys_script"
+eval "$solver_cmd"
