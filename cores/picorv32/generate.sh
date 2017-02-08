e5be5e6d428c871efbe19c8aaa943a4a63effd9c
Clifford Wolf <clifford@clifford.at>
2017-02-08 21:38:19 +0100

    Improve picorv32 generator script

--- cores/picorv32/generate.sh
+++ cores/picorv32/generate.sh
@@ -5,6 +5,7 @@ set -ex
 basedir=$PWD/../..
 picorv32v=$basedir/../picorv32/picorv32.v
 
+use_verific=false
 check_bmc_depth=20
 insn_bmc_depth=30
 
@@ -16,25 +17,56 @@ mkdir -p work
 cd work
 
 for check in reg pc imem dmem; do
-	cat > check_${check}.sby <<- EOT
-		[options]
-		mode bmc
-		depth $((check_bmc_depth))
+	{
+		cat <<- EOT
+			[options]
+			mode bmc
+			depth $((check_bmc_depth))
 
-		[engines]
-		abc bmc3
+			[engines]
+			abc bmc3
+		EOT
 
-		[script]
-		verilog_defines -D RISCV_FORMAL
-		verilog_defines -D RISCV_FORMAL_NRET=1
-		verilog_defines -D RISCV_FORMAL_XLEN=32
-		read_verilog $basedir/checks/rvfi_macros.vh
+		echo
+		if $use_verific; then
+			cat <<- EOT
+				[script]
+				verific -sv top.sv
+				verific -import testbench
 
-		read_verilog $read_verilog_picorv32_opts $picorv32v
-		read_verilog -formal $basedir/cores/picorv32/${check}check.v
-		read_verilog -formal $basedir/checks/rvfi_${check}_check.v
-		prep -nordff -top testbench
-	EOT
+				[file top.sv]
+				\`define RISCV_FORMAL
+				\`define RISCV_FORMAL_NRET 1
+				\`define RISCV_FORMAL_XLEN 32
+				\`include "rvfi_macros.vh"
+				\`include "picorv32.v"
+				\`include "${check}check.v"
+				\`include "rvfi_${check}_check.v"
+			EOT
+		else
+			cat <<- EOT
+				[script]
+				verilog_defines -D RISCV_FORMAL
+				verilog_defines -D RISCV_FORMAL_NRET=1
+				verilog_defines -D RISCV_FORMAL_XLEN=32
+				read_verilog rvfi_macros.vh
+
+				read_verilog $read_verilog_picorv32_opts picorv32.v
+				read_verilog -formal ${check}check.v
+				read_verilog -formal rvfi_${check}_check.v
+				prep -nordff -top testbench
+			EOT
+		fi
+
+		echo
+		cat <<- EOT
+			[files]
+			$picorv32v
+			$basedir/checks/rvfi_macros.vh
+			$basedir/checks/rvfi_${check}_check.v
+			$basedir/cores/picorv32/${check}check.v
+		EOT
+	} > check_${check}.sby
 
 	{
 		echo "all:: check_${check}/PASS"
@@ -63,13 +95,20 @@ for insn in $basedir/insns/insn_*.v; do
 		verilog_defines -D RISCV_FORMAL_BMC_DEPTH=${insn_bmc_depth}
 		verilog_defines -D RISCV_FORMAL_INSN=${insn}
 		verilog_defines -D RISCV_FORMAL_CHANNEL_IDX=0
-		read_verilog $basedir/checks/rvfi_macros.vh
+		read_verilog rvfi_macros.vh
 
-		read_verilog $read_verilog_picorv32_opts $picorv32v
-		read_verilog -formal $basedir/cores/picorv32/insncheck.v
-		read_verilog -formal $basedir/checks/rvfi_insn_check.v
-		read_verilog $basedir/insns/insn_${insn}.v
+		read_verilog picorv32.v
+		read_verilog insn_${insn}.v
+		read_verilog -formal insncheck.v
+		read_verilog -formal rvfi_insn_check.v
 		prep -nordff -top testbench
+
+		[files]
+		$picorv32v
+		$basedir/checks/rvfi_macros.vh
+		$basedir/checks/rvfi_insn_check.v
+		$basedir/insns/insn_${insn}.v
+		$basedir/cores/picorv32/insncheck.v
 	EOT
 
 	{
