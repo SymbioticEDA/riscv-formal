57ab1324c20f1061e1c7eec1fd31cec4a7e3dd89
Clifford Wolf <clifford@clifford.at>
2017-03-04 15:53:09 +0100

    Add C.SRAI and C.SRLI instructions

--- insns/generate.py
+++ insns/generate.py
@@ -166,6 +166,15 @@ def format_ci_lui(f):
     print("  wire [4:0] insn_rs1_rd = rvfi_insn[11:7];", file=f)
     print("  wire [1:0] insn_opcode = rvfi_insn[1:0];", file=f)
 
+def format_ci_sri(f):
+    print("", file=f)
+    print("  // CI-type instruction format (SRI variation)", file=f)
+    print("  wire [`RISCV_FORMAL_XLEN-1:0] insn_shamt = {rvfi_insn[12], rvfi_insn[6:2]};", file=f)
+    print("  wire [2:0] insn_funct3 = rvfi_insn[15:13];", file=f)
+    print("  wire [1:0] insn_funct2 = rvfi_insn[11:10];", file=f)
+    print("  wire [4:0] insn_rs1_rd = {1'b1, rvfi_insn[9:7]};", file=f)
+    print("  wire [1:0] insn_opcode = rvfi_insn[1:0];", file=f)
+
 def format_cl(f, numbytes):
     print("", file=f)
     if numbytes == 4:
@@ -535,6 +544,22 @@ def insn_c_lui(insn="c_lui"):
 
         footer(f)
 
+def insn_c_sri(insn, funct2, expr):
+    with open("insn_%s.v" % insn, "w") as f:
+        header(f, insn)
+        format_ci_sri(f)
+
+        print("", file=f)
+        print("  // %s instruction" % insn.upper(), file=f)
+        print("  wire [`RISCV_FORMAL_XLEN-1:0] result = %s;" % expr, file=f)
+        assign(f, "spec_valid", "rvfi_valid && insn_funct3 == 3'b 100 && insn_funct2 == 2'b %s && insn_opcode == 2'b 01 && insn_shamt && !insn_shamt[5]" % funct2)
+        assign(f, "spec_rs1_addr", "insn_rs1_rd")
+        assign(f, "spec_rd_addr", "insn_rs1_rd")
+        assign(f, "spec_rd_wdata", "spec_rd_addr ? result : 0")
+        assign(f, "spec_pc_wdata", "rvfi_pc_rdata + 2")
+
+        footer(f)
+
 
 ## Base Integer ISA (I)
 
@@ -597,6 +622,8 @@ insn_c_jal("c_jal", "001", True)
 insn_c_li()
 insn_c_addi16sp()
 insn_c_lui()
+insn_c_sri("c_srli", "00", "rvfi_rs1_rdata >> insn_shamt")
+insn_c_sri("c_srai", "01", "$signed(rvfi_rs1_rdata) >>> insn_shamt")
 insn_c_jal("c_j", "101", False)
 
 ## ISA Listings
---
+++ insns/insn_c_srli.v
@@ -0,0 +1,46 @@
+// DO NOT EDIT -- auto-generated from generate.py
+
+module rvfi_insn_c_srli (
+  input                                rvfi_valid,
+  input [                32   - 1 : 0] rvfi_insn,
+  input [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_pc_rdata,
+  input [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs1_rdata,
+  input [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs2_rdata,
+  input [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_mem_rdata,
+
+  output                                spec_valid,
+  output                                spec_trap,
+  output [                       4 : 0] spec_rs1_addr,
+  output [                       4 : 0] spec_rs2_addr,
+  output [                       4 : 0] spec_rd_addr,
+  output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_rd_wdata,
+  output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_pc_wdata,
+  output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_mem_addr,
+  output [`RISCV_FORMAL_XLEN/8 - 1 : 0] spec_mem_rmask,
+  output [`RISCV_FORMAL_XLEN/8 - 1 : 0] spec_mem_wmask,
+  output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_mem_wdata
+);
+
+  // CI-type instruction format (SRI variation)
+  wire [`RISCV_FORMAL_XLEN-1:0] insn_shamt = {rvfi_insn[12], rvfi_insn[6:2]};
+  wire [2:0] insn_funct3 = rvfi_insn[15:13];
+  wire [1:0] insn_funct2 = rvfi_insn[11:10];
+  wire [4:0] insn_rs1_rd = {1'b1, rvfi_insn[9:7]};
+  wire [1:0] insn_opcode = rvfi_insn[1:0];
+
+  // C_SRLI instruction
+  wire [`RISCV_FORMAL_XLEN-1:0] result = rvfi_rs1_rdata >> insn_shamt;
+  assign spec_valid = rvfi_valid && insn_funct3 == 3'b 100 && insn_funct2 == 2'b 00 && insn_opcode == 2'b 01 && insn_shamt && !insn_shamt[5];
+  assign spec_rs1_addr = insn_rs1_rd;
+  assign spec_rd_addr = insn_rs1_rd;
+  assign spec_rd_wdata = spec_rd_addr ? result : 0;
+  assign spec_pc_wdata = rvfi_pc_rdata + 2;
+
+  // default assignments
+  assign spec_rs2_addr = 0;
+  assign spec_trap = 0;
+  assign spec_mem_addr = 0;
+  assign spec_mem_rmask = 0;
+  assign spec_mem_wmask = 0;
+  assign spec_mem_wdata = 0;
+endmodule
