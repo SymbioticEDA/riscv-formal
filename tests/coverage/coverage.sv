a57acc902aa2097fe73434c850e87d0adb1181cd
Clifford Wolf <clifford@clifford.at>
2017-05-08 15:46:15 +0200

    Add tests/coverage/

---
+++ tests/coverage/coverage.sv
@@ -0,0 +1,16 @@
+module coverage(input [31:0] insn);
+	wire [`ISA_COVERAGE_LEN_RV32I-1:0] insn_valid_rv32i;
+	wire [`ISA_COVERAGE_LEN_RV32IC-1:0] insn_valid_rv32ic;
+
+	isa_coverage_rv32i  isa_coverage_rv32i_inst  (.insn(insn), .valid(insn_valid_rv32i ));
+	isa_coverage_rv32ic isa_coverage_rv32ic_inst (.insn(insn), .valid(insn_valid_rv32ic));
+
+	always_comb begin
+		// check one-hot conditions
+		assert(insn_valid_rv32i  == (insn_valid_rv32i  & -insn_valid_rv32i ));
+		assert(insn_valid_rv32ic == (insn_valid_rv32ic & -insn_valid_rv32ic));
+
+		// check insn hierarchy
+		if (insn_valid_rv32i) assert(insn_valid_rv32ic);
+	end
+endmodule
---
+++ tests/coverage/generate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def handle_isa(isa):
+    with open("../../insns/isa_%s.txt" % isa, "r") as f:
+        insns = f.read().split()
+
+    with open("isa_coverage_%s.v" % isa, "w") as f:
+        print("// DO NOT EDIT -- auto-generated from generate.py", file=f)
+        print("", file=f)
+
+        print("`define ISA_COVERAGE_LEN_%s %d" % (isa.upper(), len(insns)), file=f)
+        print("module isa_coverage_%s (input [31:0] insn, output [%d:0] valid);" % (isa, len(insns)-1), file=f)
+
+        for index, insn in enumerate(sorted(insns)):
+            print("  rvfi_insn_%s insn_%s (" % (insn, insn), file=f)
+            print("    .rvfi_valid(1'b1),", file=f)
+            print("    .rvfi_insn(insn),", file=f)
+            print("    .rvfi_pc_rdata(32'h00000000),", file=f)
+            print("    .rvfi_rs1_rdata(32'h00000000),", file=f)
+            print("    .rvfi_rs2_rdata(32'h00000000),", file=f)
+            print("    .rvfi_mem_rdata(32'h00000000),", file=f)
+            print("    .spec_valid(valid[%d])," % index, file=f)
+            print("  );", file=f)
+            
+        print("endmodule", file=f)
+
+handle_isa("rv32i")
+handle_isa("rv32ic")
