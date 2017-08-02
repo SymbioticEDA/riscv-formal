0f9d9d280067dc66a68279621d1b8fef3b6aa1f8
Clifford Wolf <clifford@clifford.at>
2017-08-02 14:58:15 +0200

    Move implementation of rocket "NO_SYSTEM" feature to wrapper.sv

--- cores/rocket/insncheck.sv
+++ cores/rocket/insncheck.sv
@@ -17,38 +17,6 @@ module testbench (
 	wire reset = cycle <= 5;
 	wire enable = cycle == `RISCV_FORMAL_BMC_DEPTH;
 
-`ifdef NO_SYSTEM
-	wire riscv_rv32i_valid_ch0;
-	wire riscv_rv32i_valid_ch1;
-
-	riscv_rv32i_insn riscv_rv32i_insn_ch0 (
-		.insn(rvfi_insn[31:0]),
-		.valid(riscv_rv32i_valid_ch0)
-	);
-
-	riscv_rv32i_insn riscv_rv32i_insn_ch1 (
-		.insn(rvfi_insn[63:32]),
-		.valid(riscv_rv32i_valid_ch1)
-	);
-
-	always @* begin
-		if (rvfi_valid[0]) begin
-			assume (!rvfi_trap[0]);
-			assume (!rvfi_halt[0]);
-			assume (!rvfi_intr[0]);
-			assume (rvfi_insn[6:0] != 7'b1110011); // no SYSTEM instructions
-			assume (riscv_rv32i_valid_ch0); // no illegal instructions
-		end
-		if (rvfi_valid[1]) begin
-			assume (!rvfi_trap[1]);
-			assume (!rvfi_halt[1]);
-			assume (!rvfi_intr[1]);
-			assume (rvfi_insn[38:32] != 7'b1110011); // no SYSTEM instructions
-			assume (riscv_rv32i_valid_ch1); // no illegal instructions
-		end
-	end
-`endif
-
 	rvfi_insn_check checker_inst (
 		.clock  (clk   ),
 		.reset  (reset ),
--- cores/rocket/wrapper.sv
+++ cores/rocket/wrapper.sv
@@ -211,6 +211,38 @@ module rocket_wrapper (
 	assign rvfi_valid = rocket.core.rvfi_mon.rvfi_valid;
 `endif
 
+`ifdef NO_SYSTEM
+	wire riscv_rv32i_valid_ch0;
+	wire riscv_rv32i_valid_ch1;
+
+	riscv_rv32i_insn riscv_rv32i_insn_ch0 (
+		.insn(rvfi_insn[31:0]),
+		.valid(riscv_rv32i_valid_ch0)
+	);
+
+	riscv_rv32i_insn riscv_rv32i_insn_ch1 (
+		.insn(rvfi_insn[63:32]),
+		.valid(riscv_rv32i_valid_ch1)
+	);
+
+	always @* begin
+		if (rvfi_valid[0]) begin
+			assume (!rvfi_trap[0]);
+			assume (!rvfi_halt[0]);
+			assume (!rvfi_intr[0]);
+			assume (rvfi_insn[6:0] != 7'b1110011); // no SYSTEM instructions
+			assume (riscv_rv32i_valid_ch0); // no illegal instructions
+		end
+		if (rvfi_valid[1]) begin
+			assume (!rvfi_trap[1]);
+			assume (!rvfi_halt[1]);
+			assume (!rvfi_intr[1]);
+			assume (rvfi_insn[38:32] != 7'b1110011); // no SYSTEM instructions
+			assume (riscv_rv32i_valid_ch1); // no illegal instructions
+		end
+	end
+`endif
+
 	(* keep *) rvfi_channel #(.CHANNEL_IDX(0)) rvfi_channel_0 (`RVFI_CONN);
 	(* keep *) rvfi_channel #(.CHANNEL_IDX(1)) rvfi_channel_1 (`RVFI_CONN);
 endmodule
