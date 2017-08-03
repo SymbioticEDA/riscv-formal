c647417b13cbfbaa42a7506266277e91387a1288
Clifford Wolf <clifford@clifford.at>
2017-08-03 14:01:33 +0200

    Improve decode.sh utility script

--- cores/rocket/decode.sh
+++ cores/rocket/decode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+case "$1" in
+	-a) mopt="-M numeric"; shift;;
+	-n) mopt="-M no-aliases"; shift;;
+	-an) mopt=""; shift;;
+	*) mopt="-M numeric,no-aliases"
+esac
 for w; do echo ".word 0x${w#0x}"; done > decode.s
 riscv32-unknown-elf-gcc -c decode.s
-riscv32-unknown-elf-objdump -d -M numeric,no-aliases decode.o | grep -A999 ^000
+riscv32-unknown-elf-objdump -d $mopt decode.o | grep -A999 ^000
 rm -f decode.s decode.o
