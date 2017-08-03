887c519907022a853e0f7bafdf9441c55783259c
Clifford Wolf <clifford@clifford.at>
2017-08-03 17:15:39 +0200

    Add randomize init to rocket testbench

--- cores/rocket/testbench.sh
+++ cores/rocket/testbench.sh
@@ -8,7 +8,7 @@ tracetb=insncheck/insn_sw_ch0/engine_0/trace_tb.v
 egrep -v 'UUT.(core.rvfi_|core.io_status_dprv|core.csr.io_time|core.csr.io_status_dprv|frontend.icache.io_resp_bits)' $tracetb > testbench.v
 
 if $use_iverilog; then
-	iverilog -o testbench -s testbench -DSIMULATION testbench.v \
+	iverilog -o testbench -s testbench -DSIMULATION -DRANDOMIZE_REG_INIT -DRANDOMIZE_MEM_INIT testbench.v \
 			rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfigWithRVFIMonitors.v \
 			rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfigWithRVFIMonitors.behav_srams.v \
 			rocket-chip/vsrc/plusarg_reader.v rocket-chip/vsrc/RVFIMonitor.v
