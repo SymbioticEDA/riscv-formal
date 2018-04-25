90950da5da5e6822579afdc4ef9fd3eec79d1968
Clifford Wolf <clifford@clifford.at>
2018-04-25 21:19:53 +0200

    Fix rvformal_addr_eq-usage in rvfi_pc_[bf]wd_check.sv
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- checks/rvfi_pc_fwd_check.sv
+++ checks/rvfi_pc_fwd_check.sv
@@ -6,6 +6,8 @@ module rvfi_pc_fwd_check (
 	reg [`RISCV_FORMAL_XLEN-1:0] expect_pc;
 	reg expect_pc_valid = 0;
 
+	wire [`RISCV_FORMAL_XLEN-1:0] pc_rdata = rvfi_pc_rdata[`RISCV_FORMAL_CHANNEL_IDX*`RISCV_FORMAL_XLEN +: `RISCV_FORMAL_XLEN];
+
 	integer channel_idx;
 	always @(posedge clock) begin
 		if (reset) begin
@@ -22,7 +24,7 @@ module rvfi_pc_fwd_check (
 				assume(rvfi_valid[`RISCV_FORMAL_CHANNEL_IDX]);
 				assume(insn_order == rvfi_order[64*`RISCV_FORMAL_CHANNEL_IDX +: 64]);
 				if (expect_pc_valid) begin
-					assert(`rvformal_addr_eq(expect_pc, rvfi_pc_rdata[`RISCV_FORMAL_CHANNEL_IDX*`RISCV_FORMAL_XLEN +: `RISCV_FORMAL_XLEN]));
+					assert(`rvformal_addr_eq(expect_pc, pc_rdata));
 				end
 			end else begin
 				for (channel_idx = 0; channel_idx < `RISCV_FORMAL_NRET; channel_idx=channel_idx+1) begin
