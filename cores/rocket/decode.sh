2fe7aaa4f4823ddf3ade4f6295a20ecd357c4bc9
Clifford Wolf <clifford@clifford.at>
2018-05-02 14:52:57 +0200

    Add 64 bit support to rocket decode.sh
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- cores/rocket/decode.sh
+++ cores/rocket/decode.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+riscv=riscv32
 case "$1" in
 	-a) mopt="-M numeric"; shift;;
 	-n) mopt="-M no-aliases"; shift;;
 	-an) mopt=""; shift;;
+	-64) riscv=riscv64; shift;;
 	*) mopt="-M numeric,no-aliases"
 esac
 for w; do echo ".word 0x${w#0x}"; done > decode.s
-riscv32-unknown-elf-gcc -c decode.s
-riscv32-unknown-elf-objdump -d $mopt decode.o | grep -A999 ^000
+${riscv}-unknown-elf-gcc -c decode.s
+${riscv}-unknown-elf-objdump -d $mopt decode.o | grep -A999 ^000
 rm -f decode.s decode.o
