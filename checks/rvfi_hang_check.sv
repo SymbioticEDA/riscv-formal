053a0ed59d9aa1251e2f1fbb10bfb69d1a24d5c5
Clifford Wolf <clifford@clifford.at>
2018-06-14 12:04:00 +0200

    Add rvfi_halt and explicit WFI handling to rvfi_hang_check.sv
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- checks/rvfi_hang_check.sv
+++ checks/rvfi_hang_check.sv
@@ -17,5 +17,11 @@ module rvfi_hang_check (
 				assert(okay);
 			end
 		end
+		for (channel_idx = 0; channel_idx < `RISCV_FORMAL_NRET; channel_idx=channel_idx+1) begin
+			if (rvfi_valid[channel_idx]) begin
+				assume(!rvfi_halt[channel_idx]);
+				assume(rvfi_insn[(channel_idx+1)*`RISCV_FORMAL_ILEN-1 : channel_idx*`RISCV_FORMAL_ILEN] != 32'b 0001000_00101_00000_000_00000_1110011); // WFI
+			end
+		end
 	end
 endmodule
