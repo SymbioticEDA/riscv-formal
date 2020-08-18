b323abe6ae617f3288b86f7c35c529ea8478247e
Donald Sebastian Leung <dl@m-labs.hk>
2020-08-18 17:29:07 +0800

    Add --nopresat flag to sby

--- checks/genchecks.py
+++ checks/genchecks.py
@@ -154,7 +154,7 @@ elif solver == "btormc":
     hargs["engine"] = "btor btormc"
     hargs["ilang_file"] = corename + "-hier.il"
 else:
-    hargs["engine"] = "smtbmc %s%s" % ("--dumpsmt2 " if dumpsmt2 else "", solver)
+    hargs["engine"] = "smtbmc --nopresat %s%s" % ("--dumpsmt2 " if dumpsmt2 else "", solver)
     hargs["ilang_file"] = corename + "-hier.il"
 
 def test_disabled(check):
